869484cf4d10d72153f1d5e2bc83adbd62f9705e
pako89 <pawel.lebioda89@gmail.com>
2013-07-14 21:09:16 +0200

    Update loc.sh for OpenCL files

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in $(find . -name "*.h" -or -name "*.cpp")
+for f in $(find . -name "*.h" -or -name "*.cpp" -or -name "*.cl")
 do 
 	wc -l $f
 done | awk '{sum += $1; } END { print sum }'
